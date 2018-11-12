6c151ef60bfc32ddf0ccc18debf70ce2877a6287
Long Ren <longren@amazon.com>
2018-11-12 11:11:33 -0800

    adding helloworld lambda function

---
+++ application-load-balancer-serverless-app/helloworld/helloworld_27.py
@@ -0,0 +1,20 @@
+def lambda_handler(event, context):
+	print '\n==event=='
+	print event
+
+	response = {
+		"statusCode": 200,
+		"statusDescription": "HTTP OK",
+		"headers": {
+		"server": "my-server",
+		"set-cookie": "name=value",
+		"Content-Type": "text/html; charset=utf-8"
+		},
+		"isBase64Encoded": False
+		}
+
+	data = "Hello World from Lambda\n"
+	response['body'] = data
+	print '===Response==\n'
+	print response
+	return response
