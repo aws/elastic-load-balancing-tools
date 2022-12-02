09ce27cf7b7b69311bb3dc9943af591423e68c78
Long Ren <longren@amazon.com>
2022-12-01 19:05:51 -0800

    to support python3

--- classic-load-balancer-consolelink-utility/consolelink_classic_load_balancer.py
+++ classic-load-balancer-consolelink-utility/consolelink_classic_load_balancer.py
@@ -21,13 +21,12 @@ import sys
 import botocore
 import csv
 
-
 # Classic Load Balancer Console Link utility
 #  version 1.0.0 2017
 # Authors: Long Ren
 
 # This script help create a spreadsheet of Classic Load Balancers' AWS console URL link along with other attributes
-#such as 'Name', 'DNSName', 'Scheme', 'HostedZoneID', 'CreatedTime',
+# such as 'Name', 'DNSName', 'Scheme', 'HostedZoneID', 'CreatedTime',
 # 'VPCId', 'AvailabilityZones', 'EC2Platform', 'Subnets', 'SecurityGroup'
 
 
@@ -48,13 +47,7 @@ import csv
 VERSION = '1.0.0'
 CONSOLE_PREFIX = 'https://console.aws.amazon.com/ec2/v2/home?region='
 
-# raw_input is now called input in python3, this allows backward compatability
-try:
-    input = raw_input
-except NameError:
-    pass
-
-#Log will be stored in CLBConsoleLink.log file in the same directory as this utility script
+# Log will be stored in CLBConsoleLink.log file in the same directory as this utility script
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -70,11 +63,10 @@ logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 
-
 def get_elb_data(region):
-    '''
+    """
     Describe the Classic Load Balancer and retrieve attributes
-    '''
+    """
     if debug:
         logger.debug("Getting existing Classic Load Balancer data")
     elbc = boto3.client('elb', region_name=region)
@@ -85,10 +77,10 @@ def get_elb_data(region):
         logger.error(e.response['Error']['Message'])
     elb_data = []
     for describe_load_balancers in paginator.paginate():
-    # Render a dictionary that contains the Classic Load Balancer attributes
+        # Render a dictionary that contains the Classic Load Balancer attributes
         for lb in describe_load_balancers['LoadBalancerDescriptions']:
             elb_item = {}
-            elb_item['DNSName']= lb['DNSName']
+            elb_item['DNSName'] = lb['DNSName']
             elb_item['Scheme'] = lb['Scheme']
             elb_item['HostedZoneID'] = lb['CanonicalHostedZoneNameID']
             elb_item['Name'] = lb['LoadBalancerName']
@@ -114,7 +106,6 @@ def get_elb_data(region):
     return elb_data
 
 
-
 def get_csv(elb_data):
     '''
     Generate a CSV file with Classic Load Balancers' Attributes and ConsoleLink
@@ -124,13 +115,14 @@ def get_csv(elb_data):
         writer = csv.writer(csv_file)
         writer.writerow(fileds)
         for lb in elb_data:
-            writer.writerow([lb.get(col,None) for col in fileds])
+            writer.writerow([lb.get(col, None) for col in fileds])
             writer.writerow([lb.get(col, None) for col in fileds])
 
+
 def get_html(elb_data):
-    '''
+    """
     Generate a html file with Classic Load Balancers' Attributes and ConsoleLink
-    '''
+    """
     html = """<html><body><title>Classic Load Balancer Console Link</title><table border="1"><tr>"""
     colunm_names = sorted(list(set(k for d in elb_data for k in d)))
     for colunm in colunm_names:
@@ -138,9 +130,9 @@ def get_html(elb_data):
     html += "</tr><tr>"
     for lb in elb_data:
         for attribute in ([lb.get(col, None) for col in colunm_names]):
-            if isinstance(attribute,str):
+            if isinstance(attribute, str):
                 if CONSOLE_PREFIX in attribute:
-                    html+='''<td><a href="{}">{}</a></td>'''.format(attribute,attribute.split('=')[-1])
+                    html += '''<td><a href="{}">{}</a></td>'''.format(attribute, attribute.split('=')[-1])
                 else:
                     html += "<td>{}</td>".format(attribute)
     html = """<!DOCTYPE html><html><title>Classic Load Balancer Console Link</title><body><table border="1"><tr>"""
@@ -152,25 +144,24 @@ def get_html(elb_data):
         html += "<tr>"
         for attribute in ([lb.get(col, None) for col in column_names]):
             if isinstance(attribute, str) and (CONSOLE_PREFIX in attribute):
-                html+='''<td><a href="{}">{}</a></td>'''.format(attribute, attribute.split('=')[-1])
+                html += '''<td><a href="{}">{}</a></td>'''.format(attribute, attribute.split('=')[-1])
             else:
                 html += "<td>{}</td>".format(attribute)
         html += "</tr>"
     html += "</table></body></html>"
-    with open('CLBConsoleLink.html', 'wb') as html_file:
+    with open('CLBConsoleLink.html', 'w') as html_file:
         html_file.write(html)
 
 
-
 def main():
-    '''
+    """
     Taking in args in main function
-    '''
+    """
     parser = argparse.ArgumentParser(
         description='Create a Console Link Spreadsheet for '
                     'Classic Load Balancers', usage='%(prog)s --region --format ')
     parser.add_argument("--region", help="The region of the Classic Load Balancers "
-                                         "that you want to describe",required=True)
+                                         "that you want to describe", required=True)
     parser.add_argument("--format", help="The format of the output file that you "
                                          "want to retrieve. Current "
                                          "supported formats are CSV and HTML", required=True)
@@ -198,7 +189,7 @@ def main():
         get_csv(elb_data)
     if format == 'html':
         get_html(elb_data)
+
+
 if __name__ == '__main__':
     main()
-
-
